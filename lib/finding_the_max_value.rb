82469e39f639c617e17f8322fc06cb349ebc56b5
Seth Cohen <smcohen82@gmail.com>
2021-02-06 23:36:01 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,5 @@
+
+
 def find_max_value(array)
   max = nil 
   array.length.times do |i|
@@ -8,6 +10,3 @@ def find_max_value(array)
   max
 end
 
-nums = [1, 4, 6, 2, 3]
-
-puts find_max_value(nums)
